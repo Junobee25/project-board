f6d3c7c4100982a5affe57ddb55c22307f203e63
Junobee25 <wnsdhqo@naver.com>
2023-12-01 12:46:54 +0900

    #18 - 시큐리티 login 경로 접근 안되는 문제 해결
    - aa29a8e 에 시큐리티 버전 이슈로 로그인 경로 테스트 안되는 부분 해결

--- src/main/java/com/fastcampus/projectboardstart/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboardstart/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.fastcampus.projectboardstart.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,8 +11,9 @@ public class SecurityConfig {
     // TODO: 모든 부분에서 인증 열어 놨지만 시큐리티 버전 문제로 해당 코드 작성시 /login, /loginout 경로 요청에서 Error 발생
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .build();
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
     }
 }
