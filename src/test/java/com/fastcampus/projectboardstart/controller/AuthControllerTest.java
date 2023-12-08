b76d445da24a5d7985765ce5c43b6e07ca9283a7
Junobee25 <wnsdhqo@naver.com>
2023-12-08 17:22:04 +0900

    #48 - AuthControllerTest: Void.class 표현 개
    원래 이 자리에는 아무 클래스가 아니라 빈이 들어가야 한다.
    `Void`는 스프링 빈이 아니므로 정확한 표현이 아니고
    일부 개발 환경에서 에러를 유발하는 것을 확인
    이에 테스트 전용 빈을 제대로 만들어서 넣는 방법으로 개선.
    테스트 빈은 아무 일도 하지 않는 것이 목적이기 때문에
    안에 내용을 가지고 있지 않다.
    다른 테스트에서 재활용할 가능성이 적은 점을 고려해
    inner class 로 작성.

--- src/test/java/com/fastcampus/projectboardstart/controller/AuthControllerTest.java
+++ src/test/java/com/fastcampus/projectboardstart/controller/AuthControllerTest.java
@@ -1,40 +1,58 @@
 package com.fastcampus.projectboardstart.controller;
 
 import com.fastcampus.projectboardstart.config.SecurityConfig;
+import com.fastcampus.projectboardstart.service.ArticleService;
+import com.fastcampus.projectboardstart.service.PaginationService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 @DisplayName("View 컨트롤러 - 인증")
-@WebMvcTest(Void.class)
 @Import(SecurityConfig.class)
-public class AuthControllerTest {
+@WebMvcTest(AuthControllerTest.EmptyController.class)
+class AuthControllerTest {
 
     private final MockMvc mvc;
 
-    public AuthControllerTest(@Autowired MockMvc mvc) {
+    @MockBean
+    private ArticleService articleService;
+    @MockBean private PaginationService paginationService;
+
+    AuthControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
     @DisplayName("[view] [GET] 로그인 페이지 - 정상 호출")
     @Test
-    public void givenNoting_whenTryingToLogIn_thenReturnsLogInView() throws Exception {
+    void givenNoting_whenTryingToLogIn_thenReturnsLogInView() throws Exception {
         // given
 
         // when & then
         mvc.perform(get("/login"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+
+        then(articleService).shouldHaveNoInteractions();
+        then(paginationService).shouldHaveNoMoreInteractions();
+    }
+
+    /**
+     * 어떤 컨트롤러도 필요하지 않은 테스트임을 나타내기 위해 테스트용 빈 컴포넌트를 사용함.
+     */
+    @TestComponent
+    static class EmptyController {
     }
 }
