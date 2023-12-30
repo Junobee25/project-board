43892f10704442b6ad40303bf9b62deb68e9256d
Junobee25 <wnsdhqo@naver.com>
2023-12-30 20:56:03 +0900

    #59 - 대댓글 비즈니스 로직 구현
    부모/자식 관계를 만들면서 entity <-> dto 간 변환 로직,
    댓글 서비스에서 댓글과 대댓글을 저장하고 삭제하는 방법,
    댓글과 대댓글을 db 데이터로부터 변환할 때
    서로 계층 구조를 만들고 각각 정렬하는 방법을 구현

--- src/main/java/com/fastcampus/projectboardstart/dto/ArticleCommentDto.java
+++ src/main/java/com/fastcampus/projectboardstart/dto/ArticleCommentDto.java
@@ -14,19 +14,24 @@ public record ArticleCommentDto(
         Long id,
         Long articleId,
         UserAccountDto userAccountDto,
+        Long parentCommentId,
         String description,
         LocalDateTime createdAt,
         String createdBy,
         LocalDateTime modifiedAt,
         String modifiedBy
-        ) {
+) {
 
     public static ArticleCommentDto of(Long articleId, UserAccountDto userAccountDto, String description) {
-        return new ArticleCommentDto(null, articleId, userAccountDto, description, null, null,null, null);
+        return ArticleCommentDto.of(articleId, userAccountDto, null, description);
     }
 
-    public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto, String description, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
-        return new ArticleCommentDto(id, articleId, userAccountDto, description, createdAt, createdBy, modifiedAt, modifiedBy);
+    public static ArticleCommentDto of(Long articleId, UserAccountDto userAccountDto, Long parentCommentId, String description) {
+        return ArticleCommentDto.of(null, articleId, userAccountDto, parentCommentId, description, null, null, null, null);
+    }
+
+    public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto, Long parentCommentId, String description, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleCommentDto(id, articleId, userAccountDto, parentCommentId, description, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
     public static ArticleCommentDto from(ArticleComment entity) {
@@ -34,6 +39,7 @@ public record ArticleCommentDto(
                 entity.getId(),
                 entity.getArticle().getId(),
                 UserAccountDto.from(entity.getUserAccount()),
+                entity.getParentCommentId(),
                 entity.getDescription(),
                 entity.getCreatedAt(),
                 entity.getCreatedBy(),
--- src/main/java/com/fastcampus/projectboardstart/service/ArticleCommentService.java
+++ src/main/java/com/fastcampus/projectboardstart/service/ArticleCommentService.java
@@ -38,7 +38,15 @@ public class ArticleCommentService {
         try {
             Article article = articleRepository.getReferenceById(dto.articleId());
             UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
-            articleCommentRepository.save(dto.toEntity(article, userAccount));
+            ArticleComment articleComment = dto.toEntity(article, userAccount);
+
+            if (dto.parentCommentId() != null) {
+                ArticleComment parentComment = articleCommentRepository.getReferenceById(dto.parentCommentId());
+                parentComment.addChildComment(articleComment);
+            } else {
+                articleCommentRepository.save(articleComment);
+            }
+
         } catch (EntityNotFoundException e) {
             log.warn("댓글 저장 실패. 댓글 작성에 필요한 정보를 찾을 수 없습니다 - dto: {}", dto);
         }
@@ -47,7 +55,9 @@ public class ArticleCommentService {
     public void updateArticleComment(ArticleCommentDto dto) {
         try {
             ArticleComment articleComment = articleCommentRepository.getReferenceById(dto.id());
-            if (dto.description() != null) { articleComment.setDescription(dto.description()); }
+            if (dto.description() != null) {
+                articleComment.setDescription(dto.description());
+            }
         } catch (EntityNotFoundException e) {
             log.warn("댓글 업데이트 실패. 댓글을 찾을 수 없습니다 - dto: {}", e.getLocalizedMessage());
         }
